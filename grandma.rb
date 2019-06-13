1595f87dd88c8ef3bb01e3364f7c72ab89d85b4e
Maasa Kono <konomaasa@gmail.com>
2019-06-13 00:24:29 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.capitalize
-    return "HUH?! SPEAK UP, SONNY!"
-  else phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
+    "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
